09b839fc35d404a14edac517b4d2f8b407cfbd0a
vineet <vineetbhoriya@gmail.com>
2023-08-23 15:40:00 +0530

    update in salary

--- src/components/Salary/SalaryDepartment/SalaryBySubDepartment.tsx
+++ src/components/Salary/SalaryDepartment/SalaryBySubDepartment.tsx
@@ -4,13 +4,12 @@ import { getjobProfileBySubDepartmentNameAsync } from "../../../redux/Slice/depa
 import { useDispatch, useSelector } from "react-redux";
 import { getSalaryBySubDepartmentAsync } from "../../../redux/Slice/SalarySlice";
 import Calendar from "react-calendar";
-import 'react-calendar/dist/Calendar.css';
-import CaretLeft from "../../../assets/CaretLeft.svg"
-import CaretRight from "../../../assets/CaretRight1.svg"
+import "react-calendar/dist/Calendar.css";
+import CaretLeft from "../../../assets/CaretLeft.svg";
+import CaretRight from "../../../assets/CaretRight1.svg";
 
 import LoaderGif from "../../../assets/loadergif.gif";
 
- 
 // getDepartmentByParentAsync
 function SalaryDepartment() {
   const location = useLocation();
@@ -26,15 +25,15 @@ function SalaryDepartment() {
     dispatch(getSalaryBySubDepartmentAsync(filter));
   }, []);
   const navigate = useNavigate();
-  
-  const handlerSelectedSubDepartment = (element: any) => {
-    console.log(element)
-    dispatch(getjobProfileBySubDepartmentNameAsync(filter));
-    navigate("/salary-jobProfile-department", { state: { steel: parentDepartment } });
-    setParentDepartment("")
-    
-  };
 
+  const handlerSelectedSubDepartment = (element: any) => {
+    console.log("cliked element", element);
+    dispatch(getjobProfileBySubDepartmentNameAsync(filter));
+    navigate("/salary-jobProfile-department", {
+      state: { steel: parentDepartment, sub: element.departmentName },
+    });
+    setParentDepartment("");
+  };
 
   // date range
   const [date, setDate] = useState<any>(new Date());
@@ -48,36 +47,34 @@ function SalaryDepartment() {
     jobProfileName: "",
     date: "",
     nextDate: "",
-    departmentName:parentDepartment
+    departmentName: parentDepartment,
   });
-
- 
 
   useEffect(() => {
     const currentDate = new Date(date);
     const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-    const day = String(currentDate.getDate()).padStart(2, '0');
+    const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+    const day = String(currentDate.getDate()).padStart(2, "0");
     setFilter({
       ...filter,
-      date: `${year}-${month}-${day}`
-    })
-  }, [date])
+      date: `${year}-${month}-${day}`,
+    });
+  }, [date]);
 
   useEffect(() => {
     if (nextDate) {
       const currentDate = new Date(nextDate);
       const year = currentDate.getFullYear();
-      const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-      const day = String(currentDate.getDate()).padStart(2, '0');
+      const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+      const day = String(currentDate.getDate()).padStart(2, "0");
       setFilter({
         ...filter,
-        nextDate: `${year}-${month}-${day}`
-      })
+        nextDate: `${year}-${month}-${day}`,
+      });
     }
-  }, [nextDate])
+  }, [nextDate]);
 
-  const [dateRange, setDateRange] = useState<any>([])
+  const [dateRange, setDateRange] = useState<any>([]);
   useEffect(() => {
     function getDateRange(startDate: any, endDate: any) {
       if (nextDate) {
@@ -88,29 +85,33 @@ function SalaryDepartment() {
           result.push(currentDate.toISOString().slice(0, 10));
           currentDate.setDate(currentDate.getDate() + 1);
         }
-        setDateRange([...result])
+        setDateRange([...result]);
       }
     }
-    getDateRange(filter.date, filter.nextDate)
-    dispatch(getSalaryBySubDepartmentAsync(filter))
-  }, [filter])
-
+    getDateRange(filter.date, filter.nextDate);
+    dispatch(getSalaryBySubDepartmentAsync(filter));
+  }, [filter]);
 
   const formatDate = (date: any) => {
-    return date.toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' });
+    return date.toLocaleDateString("en-US", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    });
   };
 
   const tileClassName = ({ date }: any) => {
-    const localDate = new Date(date.getTime() - date.getTimezoneOffset() * 60000);
-    const formattedDate = localDate.toISOString().split('T')[0];
+    const localDate = new Date(
+      date.getTime() - date.getTimezoneOffset() * 60000
+    );
+    const formattedDate = localDate.toISOString().split("T")[0];
     if (dateRange.includes(formattedDate)) {
-      return 'bg-[#ECEDFE] text-[#FFFFFF]';
+      return "bg-[#ECEDFE] text-[#FFFFFF]";
     }
-    return '';
+    return "";
   };
 
   const loaderStatus = useSelector((state: any) => state.salary.status);
-
 
   return (
     <div className="px-10 py-8">
@@ -121,10 +122,7 @@ function SalaryDepartment() {
       <div className="py-6 mb-24 overflow-auto">
         <table className="z-0  table-fixed">
           <tbody>
-
             <tr className="bg-[#ECEDFE] cursor-pointer">
-
-
               <td className="py-4 px-5 text-sm font-medium text-[#2E2E2E] whitespace-nowrap">
                 Sub Department
               </td>
@@ -167,12 +165,11 @@ function SalaryDepartment() {
                   <tr
                     key={index}
                     onClick={() => {
-                      console.log(element)
+                      console.log(element);
                       handlerSelectedSubDepartment(element);
                     }}
                     className="cursor-pointer"
                   >
-
                     <td className="hover:underline hover:font-bold py-4 px-5 text-sm font-normal text-[#2E2E2E] whitespace-nowrap border-r border-b border-solid cursor-pointer border-[#EBEBEB]">
                       {element.departmentName}
                     </td>
@@ -199,7 +196,9 @@ function SalaryDepartment() {
                         .reduce(
                           (acc: any, job: any) =>
                             acc +
-                            (job.employeeTotalHours - (job.employeePendingHours + job.employeeWorkingHours)),
+                            (job.employeeTotalHours -
+                              (job.employeePendingHours +
+                                job.employeeWorkingHours)),
                           0
                         )
                         .toFixed(2)}
@@ -237,7 +236,6 @@ function SalaryDepartment() {
       </div>
       {/* TABLE ENDS HERE */}
 
-      
       <div className="fixed flex justify-center bg-white bottom-0 left-[270px] right-0">
         <div className="flex gap-3 items-center justify-center w-[300px] h-12 mb-10 border border-solid border-[#DEDEDE] py-4 px-5 rounded-[53px] bg-[#FAFAFA]">
           <button
@@ -245,47 +243,54 @@ function SalaryDepartment() {
               const nextDate = new Date(date);
               nextDate.setDate(date.getDate() - 1);
               setDate(nextDate);
-            }}>
+            }}
+          >
             <img src={CaretLeft} alt="" className="w-4 h-4" />
           </button>
-          {showCalender && <div className="filterCalender absolute z-20 bottom-28">
-            <Calendar
-              tileClassName={tileClassName}
-              onChange={(event) => {
-                calenderDayClicked.length === 0 ? setDate(event) : "";
-                calenderDayClicked.length === 1 ? setnextDate(event) : "";
-                if (calenderDayClicked.length < 1) {
-                  setcalenderDayClicked([...calenderDayClicked, 1]);
-                }
-              }}
-              onClickDay={() => {
-                if (calenderDayClicked.length > 0) {
-                  setShowCalender(false);
-                  setcalenderDayClicked([]);
-                }
-              }}
-              className="border border-solid border-[#DEDEDE] bg-[#FAFAFA] rounded-[7px] w-[252px] h-[280px] text-[16px]"
-              formatShortWeekday={(locale, date) => {
-                console.log(locale)
-                return ['S', 'M', 'T', 'W', 'T', 'F', 'S'][date.getDay()];
-              }} />
-          </div>}
+          {showCalender && (
+            <div className="filterCalender absolute z-20 bottom-28">
+              <Calendar
+                tileClassName={tileClassName}
+                onChange={(event) => {
+                  calenderDayClicked.length === 0 ? setDate(event) : "";
+                  calenderDayClicked.length === 1 ? setnextDate(event) : "";
+                  if (calenderDayClicked.length < 1) {
+                    setcalenderDayClicked([...calenderDayClicked, 1]);
+                  }
+                }}
+                onClickDay={() => {
+                  if (calenderDayClicked.length > 0) {
+                    setShowCalender(false);
+                    setcalenderDayClicked([]);
+                  }
+                }}
+                className="border border-solid border-[#DEDEDE] bg-[#FAFAFA] rounded-[7px] w-[252px] h-[280px] text-[16px]"
+                formatShortWeekday={(locale, date) => {
+                  console.log(locale);
+                  return ["S", "M", "T", "W", "T", "F", "S"][date.getDay()];
+                }}
+              />
+            </div>
+          )}
           <p
             onClick={() => {
               setShowCalender(!showCalender);
             }}
-            className="text-sm font-medium text-[#283093] cursor-pointer">{`${formatDate(date)} - ${nextDate ? formatDate(nextDate) : formatDate(date)}`}</p>
+            className="text-sm font-medium text-[#283093] cursor-pointer"
+          >{`${formatDate(date)} - ${
+            nextDate ? formatDate(nextDate) : formatDate(date)
+          }`}</p>
           <button
             onClick={() => {
               const nextDate = new Date(date);
               nextDate.setDate(date.getDate() + 1);
               setDate(nextDate);
-            }}>
+            }}
+          >
             <img src={CaretRight} className="w-4 h-4" alt="" />
           </button>
         </div>
       </div>
-
     </div>
   );
 }
