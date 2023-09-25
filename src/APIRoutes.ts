1afed32c7b57ba093039af419540478fa2c1c224
Prakash-b1 <pinnub23@gmail.com>
2023-09-25 17:48:56 +0530

    view docment added

--- src/APIRoutes.ts
+++ src/APIRoutes.ts
@@ -1,7 +1,7 @@
-export const apiPath = `http://localhost:5050`;
+// export const apiPath = `http://localhost:5050`;
 
 
-//  export const apiPath = ``;
+ export const apiPath = ``;
 
 // export const apiPath = `https://hrms-lix0.onrender.com`;
 
