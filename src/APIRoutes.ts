a8336591331aa97801dccd291bdd3902a343e0c9
vineet <vineetbhoriya@gmail.com>
2023-10-02 16:52:32 +0530

    A

--- src/APIRoutes.ts
+++ src/APIRoutes.ts
@@ -1,10 +1,8 @@
-
-// export const apiPath = `http://localhost:5050`;
-export const apiPath = ``;
-
+export const apiPath = `http://localhost:5050`;
+// export const apiPath = ``;
 
 // export const apiPath = `https://hrms-lix0.onrender.com`;
-export const apiPath = `https://chawlacomponents.com`;
+// export const apiPath = `https://chawlacomponents.com`;
 // export const apiPath = `https://hrms-backend-04fw.onrender.com`;
 
 // JOB PROFILE API
