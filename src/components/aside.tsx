3ee9f4f6d84f9be2c5bb1d2ab8bc96c6c898a128
v889 <72309826+v889@users.noreply.github.com>
2023-10-10 11:56:35 +0530

    changes

--- src/components/aside.tsx
+++ src/components/aside.tsx
@@ -154,7 +154,7 @@ export default function aside(props: Props) {
             className="fixed h-[24px] w-[24px] p-[5px] border rounded-full border-border-primary shadow-lg  z-30 flex items-center cursor-pointer left-[258px] top-[14%]" />
 
         )}
-        {!showSidebar &&
+       
         <aside className={`flex flex-col justify-between absolute mt-[32px] shadow-right-lg bg-white top-[62px] border-r w-[270px] h-[80%] ${showSidebar ? "translate-x-[-243px]" : "translate-x-0"
           } transition-all ease-in-out duration-500`} >
           <div className="flex flex-col gap-3 px-6">
@@ -196,9 +196,9 @@ export default function aside(props: Props) {
             </Link>
           </div>
         </aside>
-        }
+        
         {/* Your Content here */}
-       <div className={`ml-[${showSidebar ? '270px' : '0px'}] mt-[74px]`}>
+       <div className={`ml-[${!showSidebar ? '270px' : '0px'}] mt-[74px]`}>
         {props.children}
       </div>
       </div>
